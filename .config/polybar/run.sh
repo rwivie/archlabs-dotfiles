3a03755cdda97f0dbb2b7f76fa3b45e2a0dc9f7f
Ron Ivie <rwivie@gmail.com>
2022-02-22 10:04:52 -0600

    add .config/polybar/config.ini

--- .config/polybar/run.sh
+++ .config/polybar/run.sh
@@ -7,6 +7,6 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2  polybar yourbar 2>/path/to/logfile &
-polybar i3-bar 2>/home/ron/.config/polybar/log &
-polybar i3-tray 2>/home/ron/.config/polybar/log &
+polybar i3-bar 2>/home/ron/.polybar-log &
+polybar i3-tray 2>/home/ron/.polybar-log &
 
